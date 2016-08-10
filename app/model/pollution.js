7eacd7a5baf4bfa026baf27b5cf60ed06d05a753
Marcos Fortún <marcos.fortun@gmail.com>
2016-08-11 00:48:00 +0200

    Cambiados modelos

--- app/model/pollution.js
+++ app/model/pollution.js
@@ -5,31 +5,18 @@ define([], function() {
     idAttribute: "id",
 
     defaults: {
-      station: "",
-      date: "",
       hour: 0,
+      station_id: 0,
+      date: "1970-01-01",
       value: 0
     },
 
     constructor: function(attributes, options) {
       var me = this;
 
-      //TODO: arreglar scraping de mediciones (los ids de estación no coinciden)
-      /*
-      var station = "" +
-        attributes.station[0] +
-        attributes.station[1] +
-        "0" +
-        attributes.station[2] +
-        attributes.station[3] +
-        "0" +
-        attributes.station[4] +
-        attributes.station[5];
-      attributes.station = station;
-      */
-
       //Composited primary key
-      attributes.id = attributes.station + "_" + attributes.date + "_" + attributes.hour;
+      //attributes.id = attributes.station + "_" + attributes.date + "_" + attributes.hour;
+      attributes.value = (typeof attributes.value == "string") ? parseFloat(attributes.value) : attributes.value;
 
       Backbone.Model.apply(this, arguments);
     }
